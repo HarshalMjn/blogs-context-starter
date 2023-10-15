64b5a55243afcc388bf64ba7b2c5d3a5f9735dc7
HarshalMjn <mahajanh006@gmail.com>
2023-10-15 22:14:17 +0530

    #

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import { Route,Routes } from "react-router-dom";
 export default function App() {
   const { fetchBlogPosts } = useContext(AppContext);
 
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const location = useLocation();
 
   
